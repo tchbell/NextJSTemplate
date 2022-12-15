f71afff0a7d7dd4abe84c7d4f4a3f7a79f17fa87
Ted Bell <tchbell@gmail.com>
2022-12-15 14:24:24 -0500

    adding layout component, layout css module, global css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,12 @@
-import { useState } from 'react';
 import Link from 'next/link';
+import Layout from '../components/layout';
 
 export default function HomePage() {
-  return <Link></Link>;
+  const LinkText = 'To Sample Page';
+  return (
+    <Layout>
+      <h1>Home Page</h1>
+      <Link href="/sample-page/sample-page">{LinkText}</Link>
+    </Layout>
+  );
 }
--- pages/sample-page/sample-page.jsx
+++ pages/sample-page/sample-page.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import Layout from '../../components/layout';
 
 export default function SamplePage() {
   return (
     <Layout>
       <h1>Sample Page</h1>
+      <Link href="/">Home</Link>
     </Layout>
   );
 }
